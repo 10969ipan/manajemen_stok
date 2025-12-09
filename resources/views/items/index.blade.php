d60389447fa4a54b204c54e4e224a85d615f6814
10969ipan <arfianirfan48@gmail.com>
2025-12-10 02:12:56 +0700

    update

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -21,7 +21,7 @@
         
         <div class="overflow-x-auto">
             <table class="min-w-full divide-y divide-gray-200">
-                <thead class="bg-gray-50">
+                <thead class="bg-gray-50"> 
                     <tr>
                         <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Nama Barang</th>
                         <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Detail Varian (Size : Stok)</th>
